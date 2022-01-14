5c4ffa4bd542378b38f52758d80a4feb8f3e01cf
juliahpm <juliahammesmello@gmail.com>
2022-01-14 01:32:41 -0300

    teste 4 variaveis

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol:'postgres',
+  port: 5432,
   dialectOptions:{
     ssl:{
       require: true,
